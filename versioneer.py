ff48c254629bf717994117f26d357520a4d25bf3
cloudQuant <yunjinqi@gmail.com>
2024-07-05 19:56:00 +0800

    fix configparser error in python3.12

--- versioneer.py
+++ versioneer.py
@@ -409,9 +409,17 @@ def get_config_from_root(root):
     # configparser.NoOptionError (if it lacks "VCS="). See the docstring at
     # the top of versioneer.py for instructions on writing your setup.cfg .
     setup_cfg = os.path.join(root, "setup.cfg")
-    parser = configparser.SafeConfigParser()
+    try:
+        parser = configparser.SafeConfigParser()
+    except Exception as e:
+        print("parser = configparser.SafeConfigParser() error info = ", e)
+        parser = configparser.ConfigParser()
     with open(setup_cfg, "r") as f:
-        parser.readfp(f)
+        try:
+            parser.readfp(f)
+        except Exception as e:
+            print("parser.readfp(f) error info = ", e)
+            parser.read_file(f)
     VCS = parser.get("versioneer", "VCS")  # mandatory
 
     def get(parser, name):
